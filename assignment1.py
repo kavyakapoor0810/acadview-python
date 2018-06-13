50223de3a17811e465f71b5a9c2303a0e8e10e7d
meghaahuja0904 <meghaahuja0904@gmail.com>
2018-06-13 18:49:06 +0530

    my first

---
+++ assignment1.py
@@ -0,0 +1,38 @@
+question1
+
+print("hello)")
+
+
+question2
+
+a="acad"
+b="view"
+print(a+b)
+
+question3
+
+ x=input("enter name")
+ y=input("enter age")
+ z=input("enter phone no")
+ print(x)
+ print(y)
+ print(z)
+
+ question4
+
+ print("let\'s get started")
+
+
+ question5
+
+s="acadview"
+course="python"
+fees=5000
+print("%s offers course of %s and fees is %d"%(s,course,fees))
+
+question6
+
+name="tony stark"
+salary=1000000
+print('%s %d'%(name,salary))
+
